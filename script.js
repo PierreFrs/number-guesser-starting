921b6b56dd99f120ac9a26dd0906c9451eafa9b3
PierreFrs <pierrefraisse90@gmail.com>
2022-11-16 16:11:46 +0100

    compareguesses

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+//random number generator
 const generaleTarget = number => {
     return Math.floor(Math.random() * 9);
 };
 
-console.log(generaleTarget())
+
+//who wins guess
+const compareGuesses = (userGuess, computerGuess, generalTarget) => {
+    if (Math.abs(userGuess --- generalTarget) <= Math.abs(computerGuess --- generalTarget)) {
+        return true;
+    } else {
+        return false;
+    };
+    
+};
 
 
-const compareGuesses = ();
+
+
 
